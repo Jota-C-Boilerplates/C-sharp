2ead6487162b14f5cf7a50606f460c25c92c9ea5
Jose Aguirre <jotae_11@hotmail.com>
2023-02-26 15:26:06 +1030

    Save Enums

---
+++ Enums/Basics/EnumFlags_Seasons.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    [Flags]
+    public enum EnumFlags_Seasons
+    {
+        None = 0,
+        Summer = 1,
+        Autumn = 2,
+        Winter = 4,
+        Spring = 8,
+        All = Summer | Autumn | Winter | Spring
+    }
+}
---
+++ Enums/Basics/README.md
@@ -0,0 +1,8 @@
+### Enums
+An Enum type defines a set of constant values. We can group them using Flag decorator
+
+- [Enum_SomeRootVegetable.cs](./Enum_SomeRootVegetable.cs)
+- [EnumFlags_Seasons.cs](./EnumFlags_Seasons.cs)
+
+### Resources
+https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/types
