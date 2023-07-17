56178e76afe289cf51215216bb13cf2572c18c42
Jose Aguirre <jotae_11@hotmail.com>
2023-07-16 19:02:29 -0500

    Add last changes

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -7,11 +7,14 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("--------------- Exception Handling");
             Console.WriteLine("------ ArgumentException");
             Console.WriteLine(exampleArgumentException("Test", 7, 'a'));
             Console.WriteLine("------ OtherKindException");
-            exampleOtherKindOfExceptions(43000, 410);
+            exampleOtherKindOfExceptions(43000, 410);*/
+
+            exampleNestedInLoop();
         }
         // PadLeft or Right complete the length requested with an specific char
         // Better retrieve String.Empty instead of use Concatenate ""
@@ -24,13 +27,13 @@ namespace ExceptionHandling
             else
                 throw new ArgumentException("Input is long thatn requested lenght");
         }
-        static void exampleOtherKindOfExceptions(int x,int y)
+        static void exampleOtherKindOfExceptions(int x, int y)
         {
             try
             {
                 if (x > 1000)
-                { 
-                    throw new ArgumentOutOfRangeException("x","x has to be 1000 or less");
+                {
+                    throw new ArgumentOutOfRangeException("x", "x has to be 1000 or less");
                 }
                 int result = x / y;
                 Console.WriteLine($"The result is: {result}");
@@ -45,8 +48,42 @@ namespace ExceptionHandling
                 Console.WriteLine("Sorry, 1000 is the limit");
                 Console.WriteLine(e.Message);
             }
-            finally {
+            finally
+            {
                 Console.WriteLine("This code always runs");
+            }
+        }
+        static void exampleNestedInLoop()
+        {
+            int[] number = { 8, 17, 24, 5, 25 };
+            int[] divisor = { 2, 0, 0, 5 };
+
+
+            for (int j = 0; j < number.Length; j++)
+            {
+                try
+                {
+                    Console.WriteLine("Code starts");
+                    try
+                    {
+                        Console.WriteLine("Number: " + number[j] +
+                                          "\nDivisor: " + divisor[j] +
+                                          "\nQuotient: " + number[j] / divisor[j]);
+                    }
+                    // Catch block for inner try block
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Inner Try Catch Block" + ex.Message);
+                        // throw; // Forzamos detener el programa.
+                        // Great cuando ya no pongo el throw la exception se contra y continua sin que llame al Catch principal
+                    }
+                    Console.WriteLine("Continue in the loop");
+                    Console.WriteLine("Finished --------------------------------------------");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Outer Try Catch Block"+ ex.Message);
+                }
             }
         }
     }
